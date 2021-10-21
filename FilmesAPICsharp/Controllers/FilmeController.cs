e537bfa5a2a2ebb91acc0c3d34cde460f971c567
Cleriston <novaes.cleriston@gmail.com>
2021-10-21 16:46:30 -0300

    Primeiro Post

---
+++ FilmesAPICsharp/Controllers/FilmeController.cs
@@ -0,0 +1,25 @@
+﻿using FilmesAPICsharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPICsharp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+
+        
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody]Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
